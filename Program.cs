f8f98a5557f46bb11228760decba3e29cd575786
Gabriel Augusto <Avgvstvs_G@protonmail.com>
2024-01-12 20:49:21 -0300

    Commit desafio poo c#

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// TODO: Realizando os testes com as classes Nokia e Iphone
+    Console.WriteLine("Smartphone Nokia:");
+    Nokia nokia = new("0000-0000", "tijolao", "111111111", "64");
+
+    nokia.Ligar();
+    nokia.ReceberLigacao();
+    nokia.InstalarAplicativo("Whatsapp");
+
+    Console.WriteLine("\n");
+
+    Console.WriteLine("Smartphone Iphone:");
+    Iphone iphone = new("1111-1111", "13 pro", "222222222", "128");
+
+    iphone.Ligar();
+    iphone.ReceberLigacao();
+    iphone.InstalarAplicativo("Instagram");
